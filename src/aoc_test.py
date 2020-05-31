df5c4f6f2855fd3027e098e7a7c46ec7fb0630eb
bcooperstl <coop999@gmail.com>
2020-05-30 20:25:39 -0500

    Added some tester functionality

---
+++ src/aoc_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import fileutils
+
+class AocTest(object):
+    def __init__(self, day, part, filename, expected_result):
+        self.day = day
+        self.part = part
+        self.filename = filename
+        self.expected_result = expected_result
+    
+    def compare_result(self, actual_result):
+        return self.expected_result ==  actual_result
---
+++ src/aoc_tests.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import aoc_test, fileutils
+
+TEST_DIR = "../test/"
+TEST_INDEX_FILE = TEST_DIR+"test_index.txt"
+
+class AocTests(object):
+    def __init__(self):
+        self.tests = []
+        self.load_tests()
+        
+    def load_tests(self):
+        test_list = fileutils.read_as_split_strings(TEST_INDEX_FILE, ",", "#") # CSV file with comments starting with #
+        for test in test_list:
+            day = int(test[0])
+            part = int(test[1])
+            expected = int(test[3])
+            self.tests.append(aoc_test.AocTest(day, part, test[2], expected))
+    
+    def filter_tests(self, day, part):
+        return [test for test in self.tests if test.day == day and test.part == part]
+
+if __name__ == '__main__':
+    tests = AocTests()
+    for test in tests.tests:
+        print("Day", test.day, "Part", test.part, "Filename", test.filename, "Expected", test.expected_result)
