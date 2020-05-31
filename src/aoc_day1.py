c4beeb4aac786f4476610f1d686a2ba282e8ae62
bcooperstl <coop999@gmail.com>
2020-05-30 21:08:02 -0500

    Added day 1 part 1 solution

--- src/aoc_day1.py
+++ src/aoc_day1.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
 
 import aoc_day
+import fileutils
+import sys
 
 class AocDay1(aoc_day.AocDay):
     def __init__(self):
         aoc_day.AocDay.__init__(self, 1)
     
     def part1(self, filename):
-        return 1
+        total = 0
+        values = fileutils.read_as_list_of_strings(filename)
+        for value in values:
+            if value[0]=='+':
+                total+=int(value[1:])
+            elif value[0]=='-':
+                total-=int(value[1:])
+            else:
+                print("Invalid input",value)
+                sys.exit()
+        return total
