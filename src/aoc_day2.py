ea605a8bc5c124aa5ac889cf9e0bf54e4f602d3c
bcooperstl <coop999@gmail.com>
2020-05-31 13:17:22 -0500

    Added day2 part 1 code

---
+++ src/aoc_day2.py
@@ -0,0 +1,24 @@
+import aoc_day
+import fileutils
+import sys
+import collections
+
+class AocDay2(aoc_day.AocDay):
+    def __init__(self):
+        aoc_day.AocDay.__init__(self, 2)
+    
+    def part1(self, filename):
+        num_twos = 0
+        num_threes = 0
+        labels = fileutils.read_as_list_of_strings(filename)
+        for label in labels:
+            frequencies = collections.defaultdict(int)
+            for letter in label:
+                frequencies[letter] += 1
+            if 2 in frequencies.values():
+                num_twos += 1
+            if 3 in frequencies.values():
+                num_threes += 1
+        return num_twos * num_threes
+    
+    
--- src/aoc_days.py
+++ src/aoc_days.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 import aoc_day1
+import aoc_day2
 
 class AocDays(object):
     def __init__(self):
         self.programs={
                   1:aoc_day1.AocDay1(),
+                  2:aoc_day2.AocDay2(),
                  }
 
     def get(self, day):
