8d6a6d45ba4941002d6e6b4cd2f6f5afe70da8b4
bcooperstl <coop999@gmail.com>
2020-05-30 20:06:08 -0500

    Initial checkin

---
+++ src/fileutils.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+import os
+
+def read_as_list_of_strings(filename):
+    if not os.access(filename, os.F_OK):
+        print(filename, "is not a valid filename!")
+        sys.exit()
+    read_file = open(filename, "r")
+    file_contents = list(read_file.readlines())
+    read_file.close()
+    return file_contents
+
+def read_as_split_strings(filename, delimiter, comment_character):
+    result = []
+    raw_strings = read_as_list_of_strings(filename)
+    for line in raw_strings:
+        if comment_character is not ""  and not line.startswith(comment_character):
+            result.append(line.split(delimiter))
+    return result
