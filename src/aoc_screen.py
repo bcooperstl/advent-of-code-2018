ab4e6dab3a2e38477241343f99edfe67e75315cd
bcooperstl <coop999@gmail.com>
2020-06-11 14:54:04 -0500

    Added basic screen class

---
+++ src/aoc_screen.py
@@ -0,0 +1,27 @@
+import sys
+
+class AocScreen(object):
+    def __init__(self, startChar=' ', minX=-10, maxX=10, minY=-10, maxY=10):
+        self.minX = minX
+        self.maxX = maxX
+        self.minY = minY
+        self.maxY = maxY
+        self.width = maxX - minX + 1
+        self.height = maxY - minY + 1
+        self.startChar = startChar
+        self.textmap = [[startChar] * self.width for i in range(self.height)]
+        
+    def display(self):
+        for line in self.textmap:
+            print("".join(line))
+    
+    def set(self, x, y, value):
+        if not self.minX <= x <= self.maxX:
+            print("x value ",x,"is out of range(",self.minX,",",self.maxX,")")
+            sys.exit()
+        if not self.minY <= y <= self.maxY:
+            print("y value ",y,"is out of range(",self.minY,",",self.maxY,")")
+            sys.exit()
+        print("Settings (",x,",",y,") to value:",value)
+        self.textmap[y-self.minY][x-self.minX]=value
+    
