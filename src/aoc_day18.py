94528296d2bdc553d1d8212fbc1dd288887b31d2
bcooperstl <coop999@gmail.com>
2020-06-23 13:42:47 -0500

    Added day 18 part 2 solution

--- src/aoc_day18.py
+++ src/aoc_day18.py
@@ -87,4 +87,34 @@ class AocDay18(aoc_day.AocDay):
             map.display()
         counts = self.count_components(map)
         return counts["tree"]*counts["lumberyard"]
-        
+
+    def part2(self, filename, extra_args):
+        start_data = fileutils.read_as_list_of_strings(filename)
+        map = aoc_screen.AocScreen(' ')
+        map.load(start_data)
+        print("Initial map:")
+        maps = [map]
+        map.display()
+        repeat_begin=0
+        repeat_end=0
+        for minute in range(1,1000000001): # 1 to 1 billion. won't got that long
+            map = self.compute_new_map(map)
+            print("After",minute,"minutes:")
+            map.display()
+            for index, prior in enumerate(maps):
+                if map.equals(prior):
+                    repeat_begin = index
+                    repeat_end = minute
+                    break
+            maps.append(map)
+            if repeat_begin > 0 and repeat_end > 0:
+                break
+        period = repeat_end - repeat_begin
+        print("Repetition occurs from maps",repeat_begin,"and",repeat_end,"which has period length",period)
+        full_periods = (1000000000 - repeat_end) // period
+        remainder = (1000000000 - repeat_end) % period
+        print("To get to a billion, there are ",full_periods,"periods of length",period,"plus a remainder of ",remainder,"left over")
+        target_map = maps[repeat_begin+remainder]
+        counts = self.count_components(target_map)
+        return counts["tree"]*counts["lumberyard"]
+    
