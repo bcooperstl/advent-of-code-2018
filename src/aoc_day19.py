216bf4e3668895bf49e35ab2de1a28d5d727916f
bcooperstl <coop999@gmail.com>
2020-06-23 20:52:29 -0500

    wip; doesn't finish

--- src/aoc_day19.py
+++ src/aoc_day19.py
@@ -22,9 +22,9 @@ class AocDay19(aoc_day.AocDay):
         while 0 <= ip < len(program):
             registers[ip_reg]=ip
             inst = program[ip]
-            print("IP:",ip,"registers",registers,inst["opcode"],inst["A"],inst["B"],inst["C"])
+            #print("IP:",ip,"registers",registers,inst["opcode"],inst["A"],inst["B"],inst["C"])
             registers = self.assembly.run_inst(inst["opcode"], inst, registers)
-            print("   ",registers)
+            #print("   ",registers)
             ip = registers[ip_reg]+1
         return registers
         
@@ -32,6 +32,14 @@ class AocDay19(aoc_day.AocDay):
     def part1(self, filename, extra_args):
         lines = fileutils.read_as_list_of_strings(filename)
         registers = [0, 0, 0, 0, 0, 0]
+        ip_reg, program = self.parse_input(lines)
+        registers = self.run_program(program, registers, ip_reg)
+        print("Final Registers:", registers)
+        return registers[0]
+
+    def part2(self, filename, extra_args):
+        lines = fileutils.read_as_list_of_strings(filename)
+        registers = [1, 0, 0, 0, 0, 0]
         ip_reg, program = self.parse_input(lines)
         registers = self.run_program(program, registers, ip_reg)
         print("Final Registers:", registers)
