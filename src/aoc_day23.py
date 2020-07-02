178f6c9d8e494501c7434b8039783e75337e605f
bcooperstl <coop999@gmail.com>
2020-07-02 13:40:10 -0500

    work in progress on day 23 part 2. Not correct answer

--- src/aoc_day23.py
+++ src/aoc_day23.py
@@ -26,3 +26,33 @@ class AocDay23(aoc_day.AocDay):
         print("Strongest is:",strongest,"with radius")
         count = sum(1 for nanobot in nanobots if self.manhattan_distance(strongest, nanobot) <= strongest["radius"])
         return count
+
+    def num_in_range(self, target, nanobots):
+        return sum(1 for nanobot in nanobots if self.manhattan_distance(target, nanobot) <= nanobot["radius"])
+    
+    def gen_edge_targets(self, nanobot):
+        return [{"x":nanobot["x"], "y":nanobot["y"], "z":nanobot["z"]}, \
+                {"x":nanobot["x"]-nanobot["radius"], "y":nanobot["y"], "z":nanobot["z"]}, \
+                {"x":nanobot["x"]+nanobot["radius"], "y":nanobot["y"], "z":nanobot["z"]}, \
+                {"x":nanobot["x"], "y":nanobot["y"]-nanobot["radius"], "z":nanobot["z"]}, \
+                {"x":nanobot["x"], "y":nanobot["y"]+nanobot["radius"], "z":nanobot["z"]}, \
+                {"x":nanobot["x"], "y":nanobot["y"], "z":nanobot["z"]-nanobot["radius"]}, \
+                {"x":nanobot["x"], "y":nanobot["y"], "z":nanobot["z"]+nanobot["radius"]}]
+
+    def part2(self, filename, extra_args):
+        best_target = None
+        best_amount = 0
+        
+        nanobot_data = fileutils.read_as_split_strings(filename, ", ","") # space is delimiter, no comments
+        nanobots = self.parse_nanobot_data(nanobot_data)
+        origin = {"x":0, "y":0, "z":0}
+        all_targets = sorted([targets for nanobot in nanobots for targets in self.gen_edge_targets(nanobot)], key=lambda t:self.manhattan_distance(t, origin))
+        
+        for target in all_targets:
+            if self.num_in_range(target, nanobots) > best_amount:
+                best_target = target
+                best_amount = self.num_in_range(target, nanobots)
+                print("Setting best target to ",target,"with value",best_amount)
+        
+        return self.manhattan_distance(origin, best_target)
+    
